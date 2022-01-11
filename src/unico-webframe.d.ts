93daa9ce69d1177f189eabaf69df26e309479ff1
Lilian N. Silva <lilian.n.silva@accenture.com>
2022-01-11 11:33:50 -0300

    removing manually inserted typings

--- src/unico-webframe.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*';
-// declare module 'unico-webframe' {
-//     export function acessoWebFrame(): any;
-//     export function webFrameModel(): any;
-//     export function initCamera(configurations: any, callback: any, layout: any): any;
-// };
